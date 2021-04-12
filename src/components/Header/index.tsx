544acd9d98792b37baaa934037f90ba1d157aeb7
IsabellePinheiro <isabellepinheiro00@gmail.com>
2021-04-12 20:32:21 -0300

    feat: DT Money

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="dtmoney" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento de site",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-05-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Moradia",
+          amount: 1900,
+          createdAt: new Date("2021-05-09 19:00:00"),
+        },
+        {
+          id: 3,
+          title: "Investimentos",
+          type: "deposit",
+          category: "Investimentos",
+          amount: 8000,
+          createdAt: new Date("2021-05-20 09:00:00"),
+        },
+      ],
+    });
+  },
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [this.schema.all("transaction")];
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
